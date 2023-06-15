ef959500e5f7ab6e058324ae933c1cfdb8096b2b
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-15 11:53:25 -0300

    deploy vercel

--- src/Register.jsx
+++ src/Register.jsx
@@ -60,6 +60,10 @@ function Register() {
       <Header />
       <ContainerDiv>
 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13864.321459381743!2d-51.16563020159699!3d-29.688449621316085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519430fe9e6c739%3A0xf84a18d61173cf13!2sR.%20J%C3%A1guari%2C%20207%20-%20Primavera%2C%20Novo%20Hamburgo%20-%20RS%2C%2093340-040!5e0!3m2!1spt-BR!2sbr!4v1686839218027!5m2!1spt-BR!2sbr" width="600" height="450" style={{ border: '0', color: 'black' }} allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"></iframe>
+
         <h2 style={{ color: 'black' }}>
           REGISTER
         </h2>
